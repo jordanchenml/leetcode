407a8b9d6eb4cec1984879a72ed7d3dc6482de04
JordanChen <freewing1126@gmail.com>
2019-10-17 13:55:05 +0800

    Finish 5.

---
+++ 0005_LongestPalindromicSubstring.py
@@ -0,0 +1,56 @@
+'''
+Given a string s, find the longest palindromic substring in s. You may assume
+that the maximum length of s is 1000.
+
+Example 1:
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+
+Example 2:
+Input: "cbbd"
+Output: "bb"
+'''
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        for i in range(len(s)):
+            # odd case, like "aba"
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            # even case, like "abba"
+            tmp = self.helper(s, i, i + 1)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
+
+    # get the longest palindrome, l, r are the middle indexes
+    # from inner to outer
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l + 1:r]
+
+class Solution1:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        for i in range(len(s)):
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            if len(s[l + 1 : r]) > len(res):
+                res = s[l + 1 : r]
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            if len(s[l + 1 : r]) > len(res):
+                res = s[l + 1 : r]
+        return res
+
+a = Solution1()
+print(a.longestPalindrome('babad'))
