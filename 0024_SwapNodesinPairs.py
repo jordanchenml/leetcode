08991fe00db815d5a351215fefc2fb4e202845c1
JordanChen <freewing1126@gmail.com>
2019-10-24 17:58:48 +0800

    Finish 24, 102.

---
+++ 0024_SwapNodesinPairs.py
@@ -0,0 +1,43 @@
+'''
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes, only nodes itself may be c
+hanged.
+
+Example:
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+'''
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        cur = dummy
+        while cur.next or cur.next.next:
+            first = cur.next
+            second = cur.next.next
+            cur.next = second
+            first.next = second.next
+            second.next = first
+            cur = cur.next.next
+        return dummy.next
+
+
+# Recursive
+class Solution1:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        new_head = head.next.next
+        head, head.next = head.next, head
+        head.next.next = self.swapPairs(new_head)
+        return head
---
+++ 0102_BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,73 @@
+'''
+Given a binary tree, return the level order traversal of its nodes' values.
+(ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+'''
+
+from typing import List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+        q = [[root]]
+        for level in q:
+            record = []
+            for node in level:
+                if node.left:
+                    record.append(node.left)
+                if node.right:
+                    record.append(node.right)
+            if record:
+                q.append(record)
+        return [[x.val for x in level] for level in q]
+
+
+class Solution1:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        ans, level = [], [root]
+        while level:
+            ans.append([node.val for node in level])
+            temp = []
+            for node in level:
+                temp.extend([node.left, node.right])
+            level = [leaf for leaf in temp if leaf]
+        return ans
+
+
+root = TreeNode(3)
+left = TreeNode(9)
+right = TreeNode(20)
+rightLeft = TreeNode(15)
+rightRight = TreeNode(7)
+root.left = left
+root.right = right
+right.left = rightLeft
+right.right = rightRight
+
+a = Solution2()
+print(a.levelOrder(root))
